4cacc7c8dd0096da3b8f5a77af8d1af22d0736c3
Kübra Özkan <kubra@garcia.com.tr>
2020-11-07 15:10:55 +0300

    register-form

--- src/pages/Auth/Register.tsx
+++ src/pages/Auth/Register.tsx
@@ -1,97 +1,99 @@
-import React, { FormEvent, useState } from "react";
+import React, {  useState  } from "react";
 import {
-  Row,
-  Col,
-  Card,
-  CardBody,
-  Form,
-  FormGroup,
-  Input,
-  Label,
-  FormText,
-  Button,
+    Row,
+    Col,
+    Card,
+    CardBody,
+    Form,
+    FormGroup,
+    Input,
+    Label,
+    Button,
+    Spinner
 } from "reactstrap";
+import { useForm } from "react-hook-form";
+
+interface LoginFormFields {
+    name: string;
+    surname: string;
+    username: string;
+    password: string;
+}
 
 function Register() {
-  const [name, setName] = useState<string>("");
-  const [surname, setSurname] = useState<string>("");
-  const [username, setUsername] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  function handleSubmit(e: FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-    const formValues = {
-      name,
-      surname,
-      username,
-      password,
-    };
-    console.log({ formValues });
-  }
-  return (
-    <div>
-      <Row className="justify-content-center">
-        <Col xs={12} sm={9} md={6}>
-          <Card>
-            <CardBody>
-              <h1 className="text-center">Üye Ol</h1>
-              <hr />
-              <Form onSubmit={handleSubmit}>
-                <FormGroup>
-                  <Label>İsim</Label>
-                  <Input
-                    type="text"
-                    placeholder="Lütfen İsminizi Giriniz..."
-                    value={name}
-                    onChange={(e) => {
-                      setName(e.currentTarget.value);
-                    }}
-                  />
-                </FormGroup>
-                <FormGroup>
-                  <Label>Soyisim</Label>
-                  <Input
-                    type="text"
-                    placeholder="Lütfen Soy isminizi Giriniz..."
-                    value={surname}
-                    onChange={(e) => {
-                      setSurname(e.currentTarget.value);
-                    }}
-                  />
-                </FormGroup>
-                <FormGroup>
-                  <Label>Kullanıcı Adı</Label>
-                  <Input
-                    type="text"
-                    placeholder="Lütfen Kullanıcı Adı Giriniz..."
-                    value={username}
-                    onChange={(e) => {
-                      setUsername(e.currentTarget.value);
-                    }}
-                  />
-                </FormGroup>
-                <FormGroup>
-                  <Label>Parola</Label>
-                  <Input
-                    type="password"
-                    placeholder="Lütfen Parola Giriniz..."
-                    value={password}
-                    onChange={(e) => {
-                      setPassword(e.currentTarget.value);
-                    }}
-                  />
-                </FormGroup>
-                <FormGroup>
-                  <Button type="submit" size="lg" color="primary" block>
-                    Üye Ol
-                  </Button>
-                </FormGroup>
-              </Form>
-            </CardBody>
-          </Card>
-        </Col>
-      </Row>
-    </div>
-  );
+    const { register, handleSubmit, errors, reset } = useForm<LoginFormFields>();
+    const [isLoading, setLoading] = useState(false);
+
+    async function onValid(data: LoginFormFields) {
+        console.log("onValid =>", { data });
+        reset();
+    }
+
+    console.log({ errors });
+
+
+    return (
+        <div>
+            <Row className="justify-content-center">
+                <Col xs={12} sm={9} md={6}>
+                    <Card>
+                        <CardBody>
+                            <h1 className="text-center">Üye Ol</h1>
+                            <hr />
+                            <Form onSubmit={handleSubmit(onValid)}>
+                                <FormGroup>
+                                    <Label>İsim</Label>
+                                    <Input
+                                        type="text"
+                                        placeholder="Lütfen İsminizi Giriniz..."
+                                        name="name"
+                                        innerRef={register({ required: true })}
+                                        invalid={!!errors["name"]}
+                                    />
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label>Soyisim</Label>
+                                    <Input
+                                        type="text"
+                                        placeholder="Lütfen İsminizi Giriniz..."
+                                        name="surname"
+                                        innerRef={register({ required: true })}
+                                        invalid={!!errors["surname"]}
+                                    />
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label>Kullanıcı Adı</Label>
+                                    <Input
+                                        type="text"
+                                        placeholder="Lütfen İsminizi Giriniz..."
+                                        name="username"
+                                        innerRef={register({ required: true })}
+                                        invalid={!!errors["username"]}
+                                    />
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label>Parola</Label>
+                                    <Input
+                                        type="text"
+                                        placeholder="Lütfen İsminizi Giriniz..."
+                                        name="password"
+                                        innerRef={register({ required: true })}
+                                        invalid={!!errors["password"]}
+                                    />
+                                </FormGroup>
+                                <FormGroup>
+                                    <Button type="submit" size="lg" color="primary" block disabled={isLoading}>
+
+                                        {isLoading ? <Spinner /> : "Üye Ol"}
+                                    </Button>
+                                </FormGroup>
+                            </Form>
+                        </CardBody>
+                    </Card>
+                </Col>
+            </Row>
+        </div>
+    );
 }
 
 export default Register;
